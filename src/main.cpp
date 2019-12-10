7b244c7e997f13028ff385ae46eb35cacab4a20b
majakovska <majamaj@live.com>
2019-12-11 00:44:33 +0100

    glut program skelet

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,34 @@
+#include <GL/glut.h>
 #include <iostream>
+#include <utility>
+#include <vector>
+#include <array>
 #include <cstdlib>
 
-int
-main(int argc, char** argv)
+using namespace std;
+
+
+void renderScene(void) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glutSwapBuffers();
+    glutPostRedisplay();
+}
+
+
+int main(int argc, char **argv)
 {
-    std::cout << "Hello project" << std::endl;
+    srand(10);
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("BestRsProject");
+
+    glutDisplayFunc(renderScene);
+
+    glutMainLoop();
+
     return 0;
 }
